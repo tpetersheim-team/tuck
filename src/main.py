ccde3324421229afe6d9427073a10eec2f7b711d
Michael Reichenberger <mcriech@gmail.com>
2020-10-22 20:25:44 -0600

    Started robinhood.py module with robinhood functions: Login, Logout, $ order, and share order

--- src/main.py
+++ src/main.py
@@ -22,11 +22,11 @@ from PyQt5.QtWidgets import (QApplication, QBoxLayout, QGridLayout,
 # We'll need to include the other modules
 
 # Header release information
-__author__ = 'Travis Petersheim, Michael Reichenberger, and Paul Miller'
+__author__ = 'Travis Petersheim & Michael Reichenberger'
 __copyright__ = 'Copyright 2020, Friar Tuck'
 __credits__ = ['']
 __license__ = 'MIT'
-__version__ = '0.1.1'
+__version__ = '0.0.0'
 __maintainer__ = 'Travis Petersheim'
 __email__ = 'travispetersheim@gmail.com'
 __status__ = 'prototype'
